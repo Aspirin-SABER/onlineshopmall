b3e14ccfb82ffbd9bbf55f127a80edee51d2bbea
Caster <aspirin_239@163.com>
2018-04-24 19:29:04 +0800

    查询商品列表

--- onlineshop_manager/onlineshop_manager_interface/src/main/java/cn/e3mall/service/ItemService.java
+++ onlineshop_manager/onlineshop_manager_interface/src/main/java/cn/e3mall/service/ItemService.java
@@ -1,7 +1,9 @@
 package cn.e3mall.service;
 
+import cn.e3mall.commom.pojo.DataGridResult;
 import cn.e3mall.pojo.TbItem;
 
 public interface ItemService {
 	TbItem getItemById(long id);
+	DataGridResult getItemList(int page,int rows);
 }
--- onlineshop_manager/onlineshop_manager_service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
+++ onlineshop_manager/onlineshop_manager_service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
@@ -1,10 +1,17 @@
 package cn.e3mall.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
+import cn.e3mall.commom.pojo.DataGridResult;
 import cn.e3mall.mapper.TbItemMapper;
 import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemExample;
 import cn.e3mall.service.ItemService;
 
 @Service
@@ -17,6 +24,19 @@ public class ItemServiceImpl implements ItemService {
 	public TbItem getItemById(long itemId) {
 		TbItem item = itemMapper.selectByPrimaryKey(itemId);
 		return item;
+	}
+
+	@Override
+	public DataGridResult getItemList(int page, int rows) {
+		PageHelper.startPage(page, rows);
+		TbItemExample example=new TbItemExample();
+		List<TbItem> list = itemMapper.selectByExample(example);
+		DataGridResult dataGridResult=new DataGridResult();
+		PageInfo<TbItem> pageInfo=new PageInfo<>(list);
+		dataGridResult.setRows(list);
+		dataGridResult.setTotal(pageInfo.getTotal());
+		
+		return dataGridResult;
 	}
 
 }
